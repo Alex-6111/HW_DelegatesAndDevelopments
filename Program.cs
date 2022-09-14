f4aa26f12e30ef8e18b6b2b427e70af99987aea7
Admin <obekas777@gmail.com>
2022-09-14 23:22:12 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace HW_DelegatesAndDevelopments
     {
         static void Main()
         {
-        public delegate void ArrayDelegat<T>(T[] array1);
-        public delegate string F();
-        public delegate T AreaDelegat<T>(T parametr1, T parametr2, T parametr3);
+        
         }
     }
 } 
